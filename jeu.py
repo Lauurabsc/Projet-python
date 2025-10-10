442846b43c319781a92c70ab0f87928686225bc5
Laura <laura.bosc@orange.fr>
2025-10-10 09:41:06 +0200

    Instructions

--- jeu.py
+++ jeu.py
@@ -1,2 +1,9 @@
 # Projet Python
 #Salut
+
+
+# Pour enregistrer les changements : git add nom du fichier
+# Pour afficher le message expliquant les modifs : git commit -m "message"
+# git push 
+
+# Pour charger les modifications : git pull dans le terminal
