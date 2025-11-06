c5d2aa070eb67d0547f40fa7a046e503e729777d
Racelle Utulo <racelle.utulo@etu.sorbonne-universite.fr>
2025-11-06 19:13:41 +0100

    reorga

--- piece/piece.py
+++ piece/piece.py
@@ -1,4 +1,4 @@
-from porte import Porte
+from ..porte import Porte
 
 class Piece : 
     
@@ -31,27 +31,3 @@ class Piece :
 
 
 
-class EntranceHall(Piece): 
-
-    """Pièce de départ"""
-
-    def __init(self, row = 0): 
-
-        config = {
-            "nord": True,
-            "sud": False, # Mur
-            "est": True,
-            "ouest": True
-        }
-
-        super().__init__(
-            nom="Entrance Hall",
-            row=row,
-            portes_config=config,
-            cost_gemmes=0,
-            rarete=0
-        )
-
-        for porte in self.portes.values():
-            if porte is not None:
-                porte.niveau_verouillage = 0
