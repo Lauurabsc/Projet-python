0339178966cec2d80740f1674d5127163836b5be
Racelle Utulo <racelle.utulo@etu.sorbonne-universite.fr>
2025-11-07 11:51:01 +0100

    Ajout des pièces oranges

---
+++ piece/pieceorange.py
@@ -0,0 +1,147 @@
+from .piece import Piece
+from ..porte import Porte
+
+
+class Hallway(Piece): 
+    """Pièce Hallway 
+    La pièce la plus simpe, avec 4 portes
+    """
+    def __init__(self,row): 
+        config = {"nord": True, "sud": True, "est": True, "ouest": True}
+    
+        super().__init__(
+            nom="Hallway",
+            row=row,
+            portes_config=config,
+            image_path="images/Hallway.png", 
+            gemmes=0,
+            rarete=1, 
+            objet=None,
+            effet_special=None,
+            condition_placement=None,
+            couleur="orange"
+        )
+
+class SecretPassage(Piece): 
+    """Pièce 'Secret passage'
+    Pièce sans issue qui déclenche un choix de 3 pièce après selection d'une couleur
+    """
+    
+    def __init__(self, row, porte_entree): 
+
+        config = {"nord": False, "sud": False, "est": False, "ouest": False}
+        config[porte_entree] = True
+
+        super().__init__(
+            nom="Secret Passage",
+            row=row,
+            portes_config=config,
+            image_path="images/Secret_Passage.png", # Supposition
+            gemmes=0,
+            rarete=2, 
+            objet=None,
+            effet_special="Déclenche un draft de couleur spécial",
+            condition_placement=None,
+            couleur="orange"
+        )
+
+    def on_enter(self, joueur, jeu): 
+        """Effet activé à l'entrée du joueur"""
+        super().on_enter(joueur, jeu)
+
+        # A ajouter dans jeu.py : La logique du jeu (choix du livre, génération des 3 salles et des 2 sécurité doit etre dans la fonction de l'objet 'jeu')
+
+        pass
+
+
+class Foyer(Piece): 
+    """Pièce 'Foyer'
+    Effet : Déverouille toutes les portes des couloirs (pièces oranges) actuels et futurs.
+    """
+
+    def __init__(self, row): 
+        config = {"nord": True, "sud": True, "est":False, "ouest": False}
+        super().__init__(
+            nom="Foyer",
+            row=row,
+            portes_config=config,
+            image_path="images/Foyer.png", 
+            gemmes=1, 
+            rarete=3,
+            objet=None,
+            effet_special="Déverrouille toutes les portes des couloirs",
+            condition_placement=None,
+            couleur="orange"
+        )
+
+    def on_draft(self, joueur, jeu): 
+
+        """Effect activé au moment du choix
+        1. Ajoute un drpaeua permanent au joueur
+        2. Déverouille tous les couloirs déjà posés
+        """
+
+        if "effets_actifs" not in joueur : 
+            joueur.effet_actif : {}
+        joueur.effet_actifs["deverouillage_foyer"] = True
+
+        # Parcourt la grille pour dévérouiller les pièces oranges 
+        for r in jeu.grille : 
+            for piece in r : 
+                if piece and piece.couleur == "orange": 
+                    print(f"Déverrouillage des portes de {piece.nom}")
+                    for porte in piece.porte.values(): 
+                        if porte is not None : 
+                            porte.niveau_verouillage = 0 
+
+
+class GreatHall(Piece): 
+    """Pièce 'Great Hall'
+    Un grand couloir dont les portes sont verouillées sauf si le joueur possède un Foyer. 
+    """
+
+    def __init(self, row, porte_entree):
+        config = {"nord": True, "sud": True, "est": True, "ouest": True}
+
+        # Porte où le joueur est entrée pour ne pas la verouiller
+        self.porte_entree_direction = porte_entree
+
+        super().__init__(
+            nom="Great Hall",
+            row=row,
+            portes_config=config,
+            image_path="images/Great_Hall.png",
+            gemmes=1, 
+            rarete=3, 
+            objet=None,
+            effet_special="Portes verrouillées sans Foyer",
+            condition_placement=None,
+            couleur="orange"
+        )
+
+    def on_discover(self, joueur, jeu) : 
+        """
+        Effet activé à la pose d
+        Vérifie si le joueur à l'effet "Foyer". 
+        """
+
+        super().on_discover(joueur, jeu)
+
+        # Verification si le joueur a l'effet du Foyer
+
+        a_le_foyer = joueur.effet_actifs.get("deverouillage_foyer", False)
+
+        if a_le_foyer :
+
+            for porte in self.portes.values(): 
+                if porte is not None: 
+                    porte.niveau_verouillage = 0
+                else : 
+                    for direction, porte in self.portes.items(): 
+                        if porte is not None : 
+                            if direction == self.porte_entree_direction: 
+                                porte.niveau_verouillage = 0 
+                            else : 
+                                porte.niveau_verouillage = 1
+
+
