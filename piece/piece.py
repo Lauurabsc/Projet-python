54467e04c7c8f20c9a8ded3b0916e4368414e4be
Laura <laura.bosc@orange.fr>
2025-11-08 01:01:49 +0100

    Restauration fichier piece.py

--- piece/piece.py
+++ piece/piece.py
@@ -1,13 +1,19 @@
 import pygame
-from ..porte import Porte
+from porte import Porte
+
 
 TAILLE_CASE = 70
 
-class Piece : 
+
+class Piece :
     """
         Classe de base pour toute les pièces du manoir pour ses caractéristiques et son affichage.
     """
-    def __init__(self, nom, row, porte_config, image_path, gemmes=0, rarete=1, objets=None, effet_special=None, condition_placement=None, couleur=None): 
+
+
+    def __init__(self, nom, row, porte_config, gemmes=0, rarete=0, image_path=None,
+             objets=None, effet_special=None, condition_placement=None, couleur=None):
+
 
         self.nom = nom
         self.gemmes = gemmes
@@ -15,12 +21,15 @@ class Piece :
         self.effet_special = effet_special
         self.rarete = rarete
         self.condition_placement = condition_placement
-        self.couleur = couleur 
+        self.couleur = couleur
         self.image_path = image_path
+
+
 
 
         self.position =(None, None)
         self.est_decouverte = False
+
 
         # Creation des portes
         self.portes = {
@@ -30,19 +39,23 @@ class Piece :
             "ouest": None
         }
 
+
         for direction, est_presente in porte_config.items():
             if est_presente:
                 self.portes[direction] = Porte(row=row)
+
 
         # Charge l'image de la pièce
         self.image_surface = pygame.image.load(image_path)
         self.image_surface = pygame.transform.scale(self.image_surface, (TAILLE_CASE, TAILLE_CASE))
 
+
         # Gère la position en PIXELS de la pièce sur l'écran
 
+
         self.rect = self.image_surface.get_rect()
-    
-    def set_position_pixels(self, col,row): 
+   
+    def set_position_pixels(self, col,row):
         """Met à jour la position logique (grille) et
         la position d'affichage (pixels) de la pièce.
         """
@@ -51,23 +64,28 @@ class Piece :
         self.rect.y = row * TAILLE_CASE
 
 
-    def draw(self, fenetre): 
+
+
+    def draw(self, fenetre):
         """
-        Méthode pour dessiner la pièce sur la fenêtre principale. 
+        Méthode pour dessiner la pièce sur la fenêtre principale.
         """
-        if self.est_decouverte : 
+        if self.est_decouverte :
             fenetre.blit(self.image_surface, self.rect)
+
 
     def on_enter(self, joueur,jeu):
         """ Méthode appelée quand le joueur entre dans la pièce."""
         print(f"Le joueur entre dans : {self.nom}")
-        pass 
+        pass
 
-    def on_draft(self, joueur,jeu): 
+
+    def on_draft(self, joueur,jeu):
         """Méthode appelée quand le joueur choisit cette pièce"""
         pass
 
-    def on_discover(self, joueur, jeu, col, row): 
+
+    def on_discover(self, joueur, jeu, col, row):
         """Méthode appelée quand la pièce est placée sur la grille"""
         self.set_position_pixels(col,row)
         self.est_decouverte = True
