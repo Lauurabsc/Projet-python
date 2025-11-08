a362ed9104cb460b14ebdf21964997ae00f0fa0b
Racelle Utulo <racelle.utulo@etu.sorbonne-universite.fr>
2025-11-08 14:40:40 +0100

    Agradissement de l'interface

--- jeu.py
+++ jeu.py
@@ -23,22 +23,39 @@ class Jeu:
         """
         # Initialisation de pygame (affichage, clavier, etc.)
         pygame.init()
+        #Initialise le module de police
+        pygame.font.init()
 
+        # Constantes de le Fenêtre 
+        self.LARGEUR = 1280
+        self.HAUTEUR = 720
 
-        # Dimensions de la fenêtre exprimées en pixels
-        self.LARGEUR = 350
-        self.HAUTEUR = 630
-
+        #Couleur 
+        self.COULEUR_FOND = (2, 33, 79)
+        self.COULEUR_CADRE = (200, 200, 200)
 
         # Création de la fenêtre du jeu
         self.fenetre = pygame.display.set_mode((self.LARGEUR, self.HAUTEUR))
-        # Titre de la fenêtre
-        pygame.display.set_caption("Blue Prince - Version IPS")
+        pygame.display.set_caption("Blue Prince - Projet POO")
+
+        self.font_titre = pygame.font.Font(None, 32) 
+        self.font_normal = pygame.font.Font(None, 24)
 
 
         # Création du manoir
         self.manoir = Manoir()
 
+        #Création de la Surface du Manoir 
+        #Création d'une surface dédiée au manoir
+        self.taille_case_manoir = self.manoir.taille_case
+        self.largeur_manoir_px = self.manoir.colonnes * self.taille_case_manoir
+        self.hauteur_manoir_px = self.manoir.lignes * self.taille_case_manoir
+        
+        #surface de la grille et les pièces
+        self.surface_manoir = pygame.Surface((self.largeur_manoir_px, self.hauteur_manoir_px))
+        
+        # Le rectangle qui définit où dessiner cette surface sur la fenêtre principale
+        self.rect_manoir = self.surface_manoir.get_rect(topleft=(50, 50))
 
         # Pièce d'entrée
         self.entree = EntranceHall()
@@ -46,11 +63,39 @@ class Jeu:
         self.manoir.ajouter_piece(self.entree, ligne=8, colonne=2)
 
 
+        # Zones inventaire (en huat à droite)
+        self.rect_inventaire = pygame.Rect(
+            self.rect_manoir.right + 50,  
+            self.rect_manoir.top,        
+            800,                          
+            250                          
+        )
+
+        # Zones des pièces
+
+        self.rect_choix_piece = pygame.Rect(
+            self.rect_inventaire.left,    
+            self.rect_inventaire.bottom + 50, 
+            self.rect_inventaire.width,  
+            400                          
+        )
+
         # Contrôle de la boucle de jeu
         self.en_cours = True # La boucle est active
-
-
         self.clock = pygame.time.Clock()  # Régule le nombre d'images par seconde
+
+    def dessiner_titres(self): 
+        # Cadres et Titre Inventaire 
+
+        pygame.draw.rect(self.fenetre, self.COULEUR_CADRE, self.rect_inventaire, 2)
+        texte_inv = self.font_titre.render("Inventaire", True, self.COULEUR_CADRE)
+        self.fenetre.blit(texte_inv, (self.rect_inventaire.x + 10, self.rect_inventaire.y + 10))
+        
+        # Cadre et Titre Choix des pièces
+        pygame.draw.rect(self.fenetre, self.COULEUR_CADRE, self.rect_choix_piece, 2)
+        texte_choix = self.font_titre.render("Please choose a room :", True, self.COULEUR_CADRE)
+        self.fenetre.blit(texte_choix, (self.rect_choix_piece.x + 10, self.rect_choix_piece.y + 10))
+   
 
 
     def boucle_principale(self):
@@ -70,21 +115,34 @@ class Jeu:
                 if evenement.type == pygame.QUIT: # Si on clique sur la croix de la fenêtre
                     self.en_cours = False # On sort de la boucle
 
+                # Quitter en appuyant sur ECHAP
+                if evenement.type == pygame.KEYDOWN:
+                    if evenement.key == pygame.K_ESCAPE:
+                        self.en_cours = False
+
 
             # Couleur de fond = noir - accueille la grille, le joueur, etc.
-            self.fenetre.fill((0, 0, 0))
+            self.fenetre.fill(self.COULEUR_FOND)
+            
+            #Dessin du manoir
+            self.surface_manoir.fill(self.COULEUR_FOND)
 
+            #Dessin la grille sur la surface du manoir
+            self.manoir.dessiner(self.surface_manoir)
 
             # Dessin des cases composant la grille
-            self.manoir.dessiner(self.fenetre)
-
+            self.manoir.dessiner(self.surface_manoir)
 
             # Dessin des pièces
             for ligne in self.manoir.grille:
                 for piece in ligne:
                     if piece:
-                        piece.draw(self.fenetre)
+                        piece.draw(self.surface_manoir)
 
+            # Surface du Manoir sur la fenêtre principale
+            self.fenetre.blit(self.surface_manoir, self.rect_manoir)
+
+            self.dessiner_titres()
 
             # A COMPLETER
 
