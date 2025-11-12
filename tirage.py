203f033f2e2c44f8752f15ebfc5dccd9478ee06d
Racelle Utulo <racelle.utulo@etu.sorbonne-universite.fr>
2025-11-12 22:35:50 +0100

    Mise a jout de orientation des pièces

--- tirage.py
+++ tirage.py
@@ -1,5 +1,7 @@
 import random 
 from piece.pieceorange import Hallway, SecretPassage, Foyer, GreatHall
+from piece.pieceviolette import Bedrooms, GuestBedroom, ServantsQuarters, MasterBedroom
+from piece.pieces_bleues import Vault, Nook, Garage, LockerRoom, Den
 
 class Pioche : 
     """
@@ -16,14 +18,23 @@ class Pioche :
         pioche = []
 
         # Rareté 1 
-        pioche.extend([Hallway] * 10)
+        pioche.extend([Hallway] * 10)         # Orange
+        pioche.extend([Bedrooms] * 5)         # Violette
+        pioche.extend([GuestBedroom] * 5)     # Violette
+        pioche.extend([Nook] * 5)             # Bleue
+        pioche.extend([Den] * 5)              # Bleue
 
         # Rareté 2
-        pioche.extend([SecretPassage] * 5)
+        pioche.extend([SecretPassage] * 5)    # Orange
+        pioche.extend([ServantsQuarters] * 3) # Violette
+        pioche.extend([Garage] * 3)           # Bleue
 
         # Rareté 3
-        pioche.extend([Foyer] * 2)
-        pioche.extend([GreatHall] * 2)
+        pioche.extend([Foyer] * 2)            # Orange
+        pioche.extend([GreatHall] * 2)        # Orange
+        pioche.extend([MasterBedroom] * 2)    # Violette
+        pioche.extend([Vault] * 2)            # Bleue
+        pioche.extend([LockerRoom] * 2)       # Bleue
 
         # TO DO : Ajouter les autres pièces
 
