18b744856dff2565950dff49e92633c8e2c42ce6
Racelle Utulo <racelle.utulo@etu.sorbonne-universite.fr>
2025-11-12 22:38:01 +0100

    colonne

--- piece/pieceviolette.py
+++ piece/pieceviolette.py
@@ -8,15 +8,16 @@ class Bedrooms(Piece):
     """Pièce 'Bedrooms'
     Effet : +2 pas en entrant"""
     
-    def __init__(self, row, porte_entree_direction=None):
+    def __init__(self, row,col, porte_entree_direction=None):
         config = {"nord": True, "sud": True, "est": True, "ouest": True}
 
         super().__init__(
             nom="Bedrooms",
             row=row,
+            col=col,
             porte_config=config,
             porte_entree_direction=porte_entree_direction,
-            image_path="Images_Blue_Prince/Images/Bedrooms/Bedrooms.png",
+            image_path="Images_Blue_Prince/Images/Bedrooms/Bedroom.png",
             rarete=1,
             couleur="violette"
         )
@@ -34,12 +35,13 @@ class GuestBedroom(Piece):
     Effet : +10 pas si elle est selectionnée
     Sans issue"""
 
-    def __init__(self, row, porte_entree):
+    def __init__(self, row,col, porte_entree_direction):
         config = {"nord": False, "sud": False, "est": False, "ouest": False}
-        config[porte_entree] = True
+        config[porte_entree_direction] = True
         super().__init__(
             nom="Guest Bedroom",
             row=row,
+            col=col,
             porte_config=config,
             image_path="Images_Blue_Prince/Images/Bedrooms/Guest_Bedroom.png",
             rarete=1,
@@ -58,26 +60,27 @@ class ServantsQuarters(Piece):
     """Pièce 'Servants's Quarters'
     Effet : Donne des clés en fonction du nombre de chambres déjà posées"""
 
-    def __init__(self, row: int, porte_entree: str):
+    def __init__(self, row: int,col, porte_entree_direction: str):
         config = {"nord": True, "sud": True, "est": True, "ouest": True}
-        config[porte_entree] = True
+        config[porte_entree_direction] = True
 
         super().__init__(
             nom="Servant's Quarters",
             row=row,
+            col=col,
             porte_config=config,
-            image_path="Images_Blue_Prince/Images/Bedrooms/Servant%27s_Quarters.png",
+            image_path="Images_Blue_Prince/Images/Bedrooms/Servants_Quarters.png",
             rarete=2,
             objets=["2_des", "1_gemme", "detecteur_metaux", "pelle"],
             couleur="violette"
         )
    
-    def on_discover(self, joueur, jeu, col, row):
+    def on_discover(self, joueur, jeu, col, row, direction_entree):
         """
         Effet activé à la POSE de la pièce.
         On compte combien de pièces violettes sont déjà sur la grille.
         """
-        super().on_discover(joueur, jeu, col, row)
+        super().on_discover(joueur, jeu, col, row, direction_entree)
         count_chambres = 0
        
         for r in jeu.manoir.grille:
@@ -98,13 +101,14 @@ class MasterBedroom(Piece):
     """Pièce 'Master Bedroom
     Rare, se trouve dans les niveaux supérieurs"""
 
-    def __init__(self, row: int, porte_entree: str):
+    def __init__(self, row: int,col, porte_entree_direction: str):
         config = {"nord": True, "sud": True, "est": True, "ouest": True}
-        config[porte_entree] = True
+        config[porte_entree_direction] = True
 
         super().__init__(
             nom="Master Bedroom",
             row=row,
+            col=col,
             porte_config=config,
             rarete=3,
             condition_placement="niveaux_superieurs",
