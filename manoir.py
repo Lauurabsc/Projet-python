f9527d42c78ef75c995d97a6d9671ae5772734c4
Laura <laura.bosc@orange.fr>
2025-11-15 12:08:33 +0100

    Corrections dans Manoir (contructeur simplifié+ ajout surface interne)

--- manoir.py
+++ manoir.py
@@ -11,29 +11,29 @@ class Manoir:
     """
 
 
-    def __init__(self, lignes=9, colonnes=5, taille_case=70):
+    def __init__(self):
         """
         Constructeur du manoir
-        Paramètres :
-        - lignes : nombre de lignes (hauteur)
-        - colonnes : nombre de colonnes (largeur)
-        - taille_case : dimension de chaque case (en pixels)
+        Initialisation des valeurs par défaut
         """
-        self.lignes = lignes
-        self.colonnes = colonnes
-        self.taille_case = taille_case
+        self.lignes = 9
+        self.colonnes = 5
+        self.taille_case = 70
+        self.largeur = self.colonnes * self.taille_case
+        self.hauteur = self.lignes * self.taille_case
+        self.surface = pygame.Surface((self.largeur, self.hauteur))
 
 
         # Grille logique : tableau 2D contenant des None
         # Chaque none représente une case vide du manoir
-        self.grille = [[None for _ in range(colonnes)] for _ in range(lignes)]
+        self.grille = [[None for _ in range(self.colonnes)] for _ in range(self.lignes)]
 
 
         # Couleur de la grille : bleu
         self.couleur_grille = (22, 70, 158)
 
 
-    def dessiner(self, surface):
+    def dessiner(self):
         """
         Cette fonction dessine la grille du manoir sur la fenêtre.
         Chaque case est un rectangle vide délimitant les différentes pièces du manoir.
@@ -43,10 +43,8 @@ class Manoir:
                 # Coordonnées (x, y) du coin supérieur gauche
                 x = j * self.taille_case
                 y = i * self.taille_case
-
-
                 # Tracé des bordures des rectangles
-                pygame.draw.rect(surface, self.couleur_grille, (x, y, self.taille_case, self.taille_case), 1)
+                pygame.draw.rect(self.surface, self.couleur_grille, (x, y, self.taille_case, self.taille_case), 1)
 
 
     def ajouter_piece(self, piece, ligne, colonne):
@@ -54,9 +52,8 @@ class Manoir:
         Ajoute une pièce dans la grille du manoir à la position donnée.
         Les pièces de départ et d'arrivées sont fixes.
 
-
         Paramètres :
-        - piece : objet de type Piece
+        - piece : objet de classe Piece
         - ligne : position verticale dans la grille
         - colonne : position horizontale dans la grille
         """
@@ -64,10 +61,8 @@ class Manoir:
             # Ajoute la pièce dans la grille logique
             self.grille[ligne][colonne] = piece
 
-
             # Calcule la position où afficher la pièce à l’écran
             piece.rect.topleft = (colonne * self.taille_case, ligne * self.taille_case)
-
 
             # Rend la pièce visible dès qu’elle est placée
             piece.est_decouverte = True  
